2d67fd748e50eb82dd7973c5dfa98cd64170978f
Elvis Blanco Gonzalez <elvisblanco1993@gmail.com>
2022-11-21 21:19:29 -0500

    ..

--- resources/views/website/partials/nav.blade.php
+++ resources/views/website/partials/nav.blade.php
@@ -1,7 +1,10 @@
 <div class="sticky top-0 bg-white/95 text-slate-600 overflow-hidden">
     <nav class="max-w-7xl mx-auto h-16 md:h-24 px-4 sm:px-6 lg:px-8">
         <div class="h-full flex items-center justify-between">
-            <a href="{{ route('home') }}" class="text-xl text-black font-bold"><span class="py-3 px-1 bg-black text-white">la</span> bahia</a>
+            <a href="{{ route('home') }}" class="text-xl text-black font-bold">
+                {{-- <span class="py-3 px-1 bg-black text-white">la</span> bahia --}}
+                bloggist
+            </a>
             <ul class="flex items-center space-x-4 text-base">
                 <li>@livewire('web.article.query')</li>
                 <li class="text-gray-200">|</li>
